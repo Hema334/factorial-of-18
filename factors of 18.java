dec8aa66ba5054ae085ac7078d49a1a8f50758c1
Hema334 <112267828+Hema334@users.noreply.github.com>
2024-11-25 18:52:09 +0530

    Add files via upload

---
+++ factors of 18.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	     int num=18;
+	     for(int i=1;i<=18;i++)
+	     {
+	         if(num%i==0)
+	         System.out.println(i);
+	     }
+	}
+}
